767ff19012487e85023b475f778a11c7dd7b9a9f
Abhijeet Tirkey <abhijeettirkey12@gmail.com>
2025-06-07 22:07:24 +0530

    Update OrderSummary.jsx
    updated the strip public key

--- frontend/src/components/OrderSummary.jsx
+++ frontend/src/components/OrderSummary.jsx
@@ -6,7 +6,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import axios from "../lib/axios";
 
 const stripePromise = loadStripe(
-	"pk_test_51KZYccCoOZF2UhtOwdXQl3vcizup20zqKqT9hVUIsVzsdBrhqbUI2fE0ZdEVLdZfeHjeyFXtqaNsyCJCmZWnjNZa00PzMAjlcL"
+	"pk_test_51RWYJCRmB2rVIXfF88ZOPo6KziKX0jNkKZlgeeNuisZtKyuhW3nRfHj7Fi9oNkZJFS9FZMUIUwI2vr6emOxhkxT0002UtmWLo5"
 );
 
 const OrderSummary = () => {
